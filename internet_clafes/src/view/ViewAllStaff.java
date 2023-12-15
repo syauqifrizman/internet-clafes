08b014dc3f0424320b2580f0c5f0ecdaed49328d
howsen <howsen25@gmail.com>
2023-12-15 15:54:18 +0700

    update PCBookedData, add ChangeRole staff for admin

--- internet_clafes/src/view/ViewAllStaff.java
+++ internet_clafes/src/view/ViewAllStaff.java
@@ -1,13 +1,22 @@
 package view;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.function.Consumer;
+
+import helper.Helper;
 import main.MainStage;
 import model.User;
 
@@ -17,6 +26,9 @@ public class ViewAllStaff {
 	private static ViewAllStaff viewAllStaff;
 	private Scene scene;
 	private TableView<User> tv;
+	private Label newRoleLabel;
+	private TextField newRoleInput;
+//	private Button changeRoleButton;
 	
 	public static ViewAllStaff getInstance() {
 		return new ViewAllStaff();
@@ -56,12 +68,44 @@ public class ViewAllStaff {
 		TableColumn<User, String> role = new TableColumn<>("Role");
 		role.setCellValueFactory(new PropertyValueFactory<>("userRole"));
 		
-		tv.getColumns().addAll(id, username, pass, age, role);
+//		Button changeRoleButton = new Button("Change Role");
 		
-		cont.getChildren().addAll(MenuAdmin.createMenu(), tv);
+		TableColumn<User, Void> cRoleColumn = new TableColumn<>("Change Role");
+		cRoleColumn.setCellFactory(param -> new ButtonCell("Change Role", t -> {
+			try {
+				handleChange(t);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}));
+		
+		tv.getColumns().addAll(id, username, pass, age, role, cRoleColumn);
+		
+		newRoleLabel = new Label("New Role: ");
+		newRoleInput = new TextField();
+		newRoleInput.setPromptText("Input New Role for this user");
+
+		HBox additionalControls = new HBox(newRoleLabel, newRoleInput);
+
+		
+		cont.getChildren().addAll(MenuAdmin.createMenu(),additionalControls, tv);
 		
 		scene = new Scene(cont, 800, 600);
     }
+	
+	private void handleChange(User user) throws SQLException {
+	    try {
+	        String newRole = newRoleInput.getText().trim();
+	        User.ChangeUserRole(user.getUserID(), newRole);
+	        repaint(); // Refresh the table after the change
+	    } catch (NumberFormatException e) {
+	    	Helper.showAlert(AlertType.ERROR, "Invalid Role. Please enter a valid role.");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        Helper.showAlert(AlertType.ERROR, "Error changing Role.");
+	    }
+	}
 	
 	private void repaint() throws SQLException {
 	    tv.getItems().clear();
@@ -75,4 +119,29 @@ public class ViewAllStaff {
 	        }
 	    }
 	}
+	
+	public class ButtonCell extends TableCell<User, Void> {
+        private final Button actionButton;
+
+        public ButtonCell(String label, Consumer<User> action) {
+            this.actionButton = new Button(label);
+
+            actionButton.setOnAction(event -> {
+                User user = getTableRow().getItem();
+                if (user != null) {
+                    action.accept(user);
+                }
+            });
+        }
+
+        @Override
+        protected void updateItem(Void item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty) {
+                setGraphic(null);
+            } else {
+                setGraphic(actionButton);
+            }
+        }
+    }
 }
