ade4897c9fd6ea3ac8c6bde4ee335235ae2011d7
tastycarbonara <kevinaurelian1@gmail.com>
2023-12-16 16:00:01 +0700

    buat ulang userrepository

---
+++ internet_clafes/src/repository/UserRepository.java
@@ -0,0 +1,35 @@
+package repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.User;
+import sqlConnect.Connect;
+
+public class UserRepository {
+	public static User getUserDetail(Integer userID) {
+		Connect db = Connect.getConnection();
+		
+		String query = "SELECT * FROM user WHERE userID = '%d'";
+		String queryExecute = String.format(query, userID);
+		
+		ResultSet res = db.executeQuery(queryExecute);
+		
+		User getUser = null; 
+		try {
+			while(res.next()) {
+				Integer currUserID = res.getInt(1);
+				String currUsername = res.getString(2);
+				Integer currUserAge = res.getInt(4);
+				String currUserRole = res.getString(5);
+				
+				getUser = new User(currUserID, currUsername, currUserAge, currUserRole);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return getUser;
+	}
+}
